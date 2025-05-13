debc3edca90bdc3308e10a4643c870380ba4d765
Nielsowvd <vandriesscheniels@gmail.com>
2025-05-13 14:15:44 +0200

    Update script

--- build-en-start-op-poort-81.sh
+++ build-en-start-op-poort-81.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 docker build -t demo-site:latest .
 docker container rm -f demo-site-container 2>/dev/null
 docker run -dt --name demo-site-container -p 81:80 demo-site:latest
